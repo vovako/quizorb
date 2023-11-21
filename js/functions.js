9b0f0d0527430e5af1f51dc6f0cabfff0b48b069
vovako <vladimirkondaurov38@gmail.com>
2023-11-21 18:20:41 +0500

    fix

--- js/functions.js
+++
@@ -1,6 +0,0 @@
-export function toPage(page) {
-	const curPage = document.querySelector('[data-page].active')
-	const newPage = document.querySelector('[data-page="' + page + '"]')
-	curPage.classList.remove('active')
-	newPage.classList.add('active')
-}
--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
-import interfacePage from './interfacePage.js'
-import viewPage from './viewPage.js'
-const wsOrigin1 = 'wss://game-zmark.p.tnnl.in'
-const wsOrigin2 = 'ws://localhost:8080'
-const ws = new WebSocket(`${wsOrigin1}/websocket/connection`)
+window.addEventListener('load', async () => {
+	if ('serviceWorker' in self.navigator) {
+		try {
+			const reg = await self.navigator.serviceWorker.register('/sw.js')
+			console.log('Service worker success register', reg);
+		}
+		catch (e) {
+			console.log('Service worker fail register', e);
+		}
+	}
+})
 
-switch (location.pathname) {
-	case '/':
-	case '/index.html':
-		interfacePage(ws)
-		break
-	case '/view.html':
-		viewPage(ws)
-		break
-}
+var map;
+
+DG.then(function () {
+	map = DG.map('map', {
+		center: [54.98, 82.89],
+		zoom: 13
+	});
+
+	map.locate({ setView: true, watch: true })
+		.on('locationfound', function (e) {
+			DG.marker([e.latitude, e.longitude]).addTo(map);
+		})
+		.on('locationerror', function (e) {
+			DG.popup()
+				.setLatLng(map.getCenter())
+				.setContent('Доступ к определению местоположения отключён')
+				.openOn(map);
+		});
+});
